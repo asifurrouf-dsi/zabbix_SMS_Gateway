c5542ef8d30e57304f62316524fc91c6254269e4
asifurrouf-dsi <48751509+asifurrouf-dsi@users.noreply.github.com>
2019-12-12 15:56:50 +0600

    Update and rename d7sms.py to sms.py

--- sms.py
+++ sms.py
@@ -4,11 +4,6 @@
 # Python2 & 3script for sending sms through D7SMS gateway
 # http://d7networks.com
 
-USERNAME = 'API_Username'# d7 sms gateway API Username
-PASSWORD =  'API_Password'# d7 sms gateway API Password
-SOURCE_ADDRESS = 'd7-zab' # source address to be used while sending sms
-
-
 import sys
 
 if __name__ == '__main__':
@@ -22,18 +17,17 @@ if __name__ == '__main__':
     # args = parser.parse_args()
     to = sys.argv[1]
     content = sys.argv[2]
-    if not (to and content) :
+    if not (to and content):
         print("to and content arguments are required to send sms")
     else:
-        content =  content.strip('\'')
-        to = to.strip('\'').split(',') # will be a list of destinations
+        content = content.strip('\'')
+        to = to.strip('\'').split(',')  # will be a list of destinations
         for destination in to:
             baseParams = {
-                'username': USERNAME,
-                'password': PASSWORD,
-                'from': SOURCE_ADDRESS,
+                'token': '073e50fecc3ae4157214fd00d35a2251',
                 'to': destination,
-                'content': content
+                'message': content
             }
             # Send an SMS-MT to d7 sms gateway
-            urlopen("http://smsc.d7networks.com:1401/send?%s" % urlencode(baseParams)).read()
+            urlopen("http://api.greenweb.com.bd/api.php?%s" %
+                    urlencode(baseParams)).read()
